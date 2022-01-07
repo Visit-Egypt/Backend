878d24c740177ac1c267a54c08ced965af18ee6b
Zyad Yasser <35318763+Zyad1999@users.noreply.github.com>
2022-01-07 19:29:51 +0200

    try 8

--- visitegypt/api/routers/chatbot/chatbot.py
+++ visitegypt/api/routers/chatbot/chatbot.py
@@ -9,6 +9,7 @@ import spacy
 from nltk.stem.lancaster import LancasterStemmer
 import json
 import requests
+import en_core_web_sm
 
 nltk.download('punkt')
 stemmer =  LancasterStemmer()
@@ -28,7 +29,7 @@ reponses = { 0 : ["Four Seasons Hotel Cairo at the First Residence(35 Giza Stree
 8 : ["Happy to help!", "Any time!", "My pleasure" ,"You are welcome"] ,
 9: ["weather"]}
 
-model_entity =  spacy.load('en_core_web_sm')
+model_entity =  en_core_web_sm.load()
 
 router = APIRouter(responses=generate_response_for_openapi("Chatboot"))
 
