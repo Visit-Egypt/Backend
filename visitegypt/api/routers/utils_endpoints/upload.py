11a2a63f97f3b89bbf12c29eb970d01a19596e39
geekahmed <geekahmed1@gmail.com>
2022-01-08 14:01:01 +0200

    Adding upload to posts, changing upload schema, and solving problems

--- visitegypt/api/routers/utils_endpoints/upload.py
+++ visitegypt/api/routers/utils_endpoints/upload.py
@@ -6,9 +6,10 @@ from visitegypt.core.utilities.services import upload_service
 router = APIRouter(responses=generate_response_for_openapi("Upload"))
 repo = get_dependencies().upload_repo
 
+"""
 @router.post('/',  tags=["Utilities"])
 async def upload_request(upload_req: UploadRequest):
-    """
+    
         The user has to send the upload request with the following data
         {
             "user_id": "",
@@ -16,11 +17,11 @@ async def upload_request(upload_req: UploadRequest):
             "resource_name": "",
             "content_type":""
         }
-    """
+    
     try:
         return await upload_service.generate_presigned_url(repo, upload_req)
     except Exception as e: raise e
-    
+"""    
 
 # response_model= UploadResponse,
 
--- visitegypt/infra/database/repositories/upload_repository.py
+++ visitegypt/infra/database/repositories/upload_repository.py
@@ -1,13 +1,14 @@
 
 
 from visitegypt.core.accounts.entities.user import UserUpdate
+from visitegypt.core.posts.entities.post import UpdatePost
 from visitegypt.core.utilities.entities.upload import UploadConfirmation, UploadRequest, UploadResponse
-from visitegypt.infra.database.repositories import user_repository
+from visitegypt.infra.database.repositories import user_repository, post_repository
 from visitegypt.infra.database.events import db
 from visitegypt.config.environment import DATABASE_NAME, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME, AWS_ACCESS_KEY_ID
 import boto3
 import uuid
-from typing import DefaultDict
+from typing import DefaultDict, List
 
 async def generate_presigned_url(upload_req: UploadRequest) -> UploadResponse:
     # Generate S3 Client
@@ -31,14 +32,20 @@ async def generate_presigned_url(upload_req: UploadRequest) -> UploadResponse:
                                               ]
                                           )
 
-    return UploadResponse(**url, image_key = str(image_id))
+    return UploadResponse(**url)
 
 
-async def uploaded_object_urls(images_keys: DefaultDict(list)) -> bool:
+async def uploaded_object_urls(images_keys: DefaultDict(list), user_id: str) -> bool:
   try:
-    print(images_keys)
     if images_keys.get('users') != None and len(images_keys.get('users')) > 0:
       user_image : str = f'https://{AWS_S3_BUCKET_NAME}.s3.us-west-2.amazonaws.com/{images_keys.get("users")[0]}'
-      user_id : str = images_keys.get('users')[0].split('/')[2]
+      # user_id : str = images_keys.get('users')[0].split('/')[2]
       await user_repository.update_user(UserUpdate(photo_link=user_image), user_id)
+    
+    if images_keys.get('posts') != None and len(images_keys.get('posts')) > 0:
+      post_images : List = [f'https://{AWS_S3_BUCKET_NAME}.s3.us-west-2.amazonaws.com/{image}' for image in images_keys.get('posts')]
+      post_id : str = images_keys.get('posts')[0].split('/')[2]
+
+      updated_post : UpdatePost = UpdatePost(list_of_images = post_images)
+      await post_repository.update_post(updated_post, post_id, user_id)
   except Exception as e: raise e
